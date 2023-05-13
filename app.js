cd3abe48ef5c95cecca3d91c3b663bc7d6157028
abdul-hanan-1 <a.hanan.rana@gmail.com>
2023-05-13 23:55:02 +0500

    created a post request endpoint

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello from the server', app: 'Natours' });
 });
 
+app.post('/', (req, res) => {
+  res.send('You can send post request to this url.');
+});
+
 app.listen(port, () => {
   console.log('App running on port 3000');
 });
