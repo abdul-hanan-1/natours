516f25034bfdac56108dfd50250fd9e1267098b1
abdul-hanan-1 <a.hanan.rana@gmail.com>
2023-05-16 01:07:58 +0500

    added code for saving to file upon post request

--- app.js
+++ app.js
@@ -27,8 +27,21 @@ app.get('/api/v1/tours', (req, res) => {
 });
 
 app.post('/api/v1/tours', (req, res) => {
-  console.log(req.body);
-  res.send('Tour added');
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {
