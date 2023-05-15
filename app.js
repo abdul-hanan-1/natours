5b681ba65b8bfdbcd01c278ac9084ef2013de230
abdul-hanan-1 <a.hanan.rana@gmail.com>
2023-05-15 23:41:49 +0500

    added get method for tours

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
@@ -9,6 +10,19 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
   res.send('You can send post request to this url.');
+});
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tours,
+    },
+  });
 });
 
 app.listen(port, () => {
