9a9ba3ed2ee615bc779484aa87e69f48981ff9f1
abdul-hanan-1 <a.hanan.rana@gmail.com>
2023-05-21 23:48:03 +0500

    did some more refactoring

--- app.js
+++ app.js
@@ -71,13 +71,8 @@ const updateTour = (req, res) => {
   });
 };
 
-app.get('/api/v1/tours', getAllTours);
-
-app.get('/api/v1/tours/:id', getTour);
-
-app.patch('/api/v1/tours/:id', updateTour);
-
-app.post('/api/v1/tours', createTour);
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour);
 
 app.listen(port, () => {
   console.log('App running on port 3000');
