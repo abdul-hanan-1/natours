d2b0ed4718733782115018110d989a5f1a2bc4b1
abdul-hanan-1 <a.hanan.rana@gmail.com>
2023-05-21 23:42:32 +0500

    refactored routes

--- app.js
+++ app.js
@@ -5,28 +5,20 @@ const app = express();
 app.use(express.json());
 const port = '3000';
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello from the server', app: 'Natours' });
-});
-
-app.post('/', (req, res) => {
-  res.send('You can send post request to this url.');
-});
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     data: {
       tours,
     },
   });
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   console.log(req.params.id);
   const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id);
@@ -42,9 +34,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
       tour,
     },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
@@ -60,7 +52,32 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
+
+const updateTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+app.get('/api/v1/tours', getAllTours);
+
+app.get('/api/v1/tours/:id', getTour);
+
+app.patch('/api/v1/tours/:id', updateTour);
+
+app.post('/api/v1/tours', createTour);
 
 app.listen(port, () => {
   console.log('App running on port 3000');
